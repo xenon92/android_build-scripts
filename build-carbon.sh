56cbc49d1db301bf901e05aba1d1b05c0d601844
Shubhang <shubhang92@gmail.com>
2014-01-17 12:12:48 +0530

    add patch to fix double day in notification header

--- build-carbon.sh
+++ build-carbon.sh
@@ -71,6 +71,16 @@ cd ../
 
 echo -e ""
 echo -e ""
+echo -e "Fixing SystemUI double - day of the week - in notification pulldown..."
+echo -e ""
+echo -e ""
+cd frameworks/base
+git checkout .
+patch -p1 < fixdoubleday.patch
+cd ../../
+
+echo -e ""
+echo -e ""
 echo -e "Patching complete... Review to see if no errors were there.."
 echo -e ""
 read -p "Press ENTER to continue..."
