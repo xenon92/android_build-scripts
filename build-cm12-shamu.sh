98c6fc16317e5cb6f926e9cc7f495f4e43cd03aa
Shubhang Rathore <shubhang92@gmail.com>
2015-01-06 11:39:39 -0500

    shamu: add cm-12.0 build script

---
+++ build-cm12-shamu.sh
@@ -0,0 +1,71 @@
+# Android Compiling Script
+#
+# This script automates the process of compiling android
+# operating system.
+#
+# Copyright (C) 2014 Shubhang Rathore
+#
+# This script is specifically for
+# Nexus 6 CyanogenMod 12
+
+
+
+# Get time at the start of the build
+res1=$(date +%s.%N)
+
+
+# Clear screen
+clear
+
+
+echo -e ""
+echo -e "#######################################"
+echo -e "#                                     #"
+echo -e "#          BUILD ENVIRONMENT          #"
+echo -e "#                                     #"
+echo -e "#######################################"
+echo -e ""
+# Setup environment
+. build/envsetup.sh
+
+
+
+# Override flags
+export KBUILD_BUILD_USER="shubhang"
+export KBUILD_BUILD_HOST="neon"
+
+export RELEASE_TYPE=NIGHTLY
+
+
+
+echo -e ""
+echo -e "#######################################"
+echo -e "#                                     #"
+echo -e "#           LUNCHING DEVICE           #"
+echo -e "#                                     #"
+echo -e "#######################################"
+echo -e ""
+# Lunch device
+lunch cm_shamu-userdebug
+
+
+echo -e ""
+echo -e "#######################################"
+echo -e "#                                     #"
+echo -e "#              COMPILING              #"
+echo -e "#                                     #"
+echo -e "#######################################"
+echo -e ""
+
+# Start compilation
+mka bacon
+echo -e ""
+
+
+###### END OF BUILD ######
+
+
+
+# Get elapsed time
+res2=$(date +%s.%N)
+echo "${bldgrn}Total time elapsed: ${txtrst}${grn}$(echo "($res2 - $res1) / 60"|bc ) minutes ($(echo "$res2 - $res1"|bc ) seconds) ${txtrst}"
